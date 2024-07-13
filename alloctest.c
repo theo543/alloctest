344df57aa6e7c847877346255a8afaf26b2a491f
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-07-13 14:22:37 +0300

    Allocate MiB instead of GiB

--- alloctest.c
+++ alloctest.c
@@ -13,18 +13,18 @@ uint32_t fast_rng(void) {
 }
 
 int main(void) {
-    const uint64_t GB = 1ull<<30;
+    const uint64_t MB = 1ull<<20;
     int count = 0;
     char **allocs = NULL;
 
     for(;;) {
-        char *ptr = malloc(GB);
+        char *ptr = malloc(MB);
         if(ptr == NULL) {
             break;
         }
         count++;
 
-        for(uint64_t i = 0;i < GB;i += 4) {
+        for(uint64_t i = 0;i < MB;i += 4) {
             // fill 4 bytes with a random value
             uint32_t val = fast_rng();
             memcpy(ptr + i, &val, sizeof(uint32_t));
@@ -35,13 +35,13 @@ int main(void) {
 
         // print an exclamation mark check if a random byte from a random allocation is 0 so the allocations can't be optimized out
         int which_alloc = fast_rng() % count;
-        int which_byte = fast_rng() % GB;
+        int which_byte = fast_rng() % MB;
         int last_is_0 = (allocs[which_alloc][which_byte]) == 0;
         char* last_is_0_result = last_is_0 ? "!" : "";
 
-        printf("\x1b[2K\rallocated %d GiB%s", count, last_is_0_result);
+        printf("\x1b[2K\rallocated %d MiB%s", count, last_is_0_result);
         fflush(stdout);
     }
 
-    printf("\nallocation returned NULL after %d GiB\n", count);
+    printf("\nallocation returned NULL after %d MiB\n", count);
 }
